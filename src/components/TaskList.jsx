f1bfdd4adbef0100ddd2a1b67296bdbd9cb1ba45
AnkurOtaku <ankurdahiya112@gmail.com>
2024-11-14 16:09:12 +0530

    bug fixes

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,16 +3,24 @@ import TaskItem from "./TaskItem";
 
 function TaskList({ tasks, deleteTask, toggleTaskCompletion }) {
   return (
-    <ul className="space-y-2 transition-all duration-500 ease-in-out">
-        {tasks.map((task) => (
-          <TaskItem
-            key={task.id}
-            task={task}
-            deleteTask={deleteTask}
-            toggleTaskCompletion={toggleTaskCompletion}
-          />
-        ))}
-      </ul>
+    <div>
+      {tasks.length === 0 ? (
+        <div className="flex justify-center items-center h-64 bg-gradient-to-r animate-gradient-x text-white rounded-lg shadow-lg">
+        <p className="text-xl font-semibold">No tasks yet! Add some tasks to get started.</p>
+      </div>
+      ) : (
+        <ul className="space-y-2 transition-all duration-500 ease-in-out">
+          {tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              deleteTask={deleteTask}
+              toggleTaskCompletion={toggleTaskCompletion}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 
